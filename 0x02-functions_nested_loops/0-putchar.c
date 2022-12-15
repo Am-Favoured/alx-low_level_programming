87a055694de3137a2d926cc649fedec8b6869730
Am-favoured <favoureka09@gmail.com>
2022-12-14 16:03:13 -0800

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,12 +2,14 @@
 
 /**
  * main - Entry point
- * Return: Always 0
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 
 {
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -17,6 +19,7 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-        return (0);
 
-}	
+	return (0);
+
+}
